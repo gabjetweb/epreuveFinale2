ef09b14fb1759848ab3047e911a65f2711bf1108
gabjetté <gabjet.web@gmail.com>
2017-03-21 18:22:09 -0400

    connexion et création à une BDD MongoDB et tentative d'étape 3

--- etape.js
+++ etape.js
@@ -9,6 +9,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+var db // variable qui contiendra le lien sur la BD
+
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(8081, () => {
+    console.log('connexion à la BD et on écoute sur le port 8081')
+  })
+})
+
 app.get('/etape1', function (req, res) {
  		fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
 		if (err) return console.error(err);
@@ -27,12 +37,13 @@ app.get('/etape2', function (req, res) {
 })
 
 app.get('/etape3', function (req, res) {
- 	fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
-		if (err) return console.error(err);
-			obj = JSON.parse(data)
-			console.log(obj);
-		  	res.render('index.ejs', {provinces: obj});
-		});
+    var cursor = db.collection('province').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
+    // renders index.ejs
+    // affiche le contenu de la BD
+    res.render('index.ejs', {adresse: resultat})
+
+    }) 
 })
 
 app.get('/etape4', function (req, res) {
