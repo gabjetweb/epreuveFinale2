889db6d9fe37ab49bf2bebeab468768585c34d36
gabjetté <gabjet.web@gmail.com>
2017-03-21 17:38:35 -0400

    Petit ménage dans le document.

--- etape.js
+++ etape.js
@@ -1,20 +1,52 @@
-const fs = require("fs");
-const express = require('express');
-const bodyParser= require('body-parser')
-//const MongoClient = require('mongodb').MongoClient
-//const ObjectID = require('mongodb').ObjectID;
-const app = express();
-app.set('view engine', 'ejs'); // générateur de template 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))  // pour utiliser le dossier public
-app.use(bodyParser.json())  // pour traiter les données JSON
+var http = require("http");
+var ejs = require('ejs');
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-app.get('/', function (req, res) {
+app.get('/etape1', function (req, res) {
  		fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
 		if (err) return console.error(err);
-		obj = JSON.parse(data)
+		var obj = JSON.parse(data)
 		  res.end(data);
 		});
+});
+
+app.get('/etape2', function (req, res) {
+ 	fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
+		if (err) return console.error(err);
+			obj = JSON.parse(data)
+			console.log(obj);
+		  	res.render('index.ejs', {provinces: obj});
+		});
+})
+
+app.get('/etape3', function (req, res) {
+ 	fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
+		if (err) return console.error(err);
+			obj = JSON.parse(data)
+			console.log(obj);
+		  	res.render('index.ejs', {provinces: obj});
+		});
+})
+
+app.get('/etape4', function (req, res) {
+
+})
+
+app.get('/etape5', (req, res) => {
+ var id = req.params.id
+ console.log(id)
+ db.collection('adresse')
+ .findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
+
+if (err) return console.log(err)
+ res.redirect('/')  // redirige vers la route qui affiche la collection
+ })
+})
+
+app.get('/etape6', function (req, res) {
+
 })
 
 var server = app.listen(8081, function () {
@@ -22,5 +54,5 @@ var server = app.listen(8081, function () {
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
-})
-
+   console.log('Server running.');
+});
