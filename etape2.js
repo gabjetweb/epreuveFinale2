5314027debe9f329057f720310763639e8d62b21
gabjetté <gabjet.web@gmail.com>
2017-03-21 17:25:20 -0400

    insertion du tableau de l'étape 2

--- etape2.js
+++ etape2.js
@@ -1,46 +1,31 @@
-const fs = require("fs");
-const express = require('express');
-const bodyParser= require('body-parser')
-//const MongoClient = require('mongodb').MongoClient
-//const ObjectID = require('mongodb').ObjectID;
-const app = express();
-app.set('view engine', 'ejs'); // générateur de template 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))  // pour utiliser le dossier public
-app.use(bodyParser.json())  // pour traiter les données JSON
+var http = require("http");
+var ejs = require('ejs');
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-var obj;
-
-function affiche_objet(obj) {
-	var ch='<table>'
-for (propriete in  obj) {
-	ch+="<tr>"+"<td>"+propriete+'</td>'+'<td>'+obj[propriete]+'</td>'+"</tr>"
-}
-ch += '</table>'
-return ch;
-}
-
-app.get('/fichier', function (req, res) {
+app.get('/', function (req, res) {
  		fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
 		if (err) return console.error(err);
+		var obj = JSON.parse(data)
+		  res.writeHead(200, {"Content-Type": "text/html"});
 		  res.end(data);
 		});
-})
+});
 
-app.get('/province', function (req, res) {
- 		fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
+app.get('/etape2', function (req, res) {
+ 	fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
 		if (err) return console.error(err);
-		obj = JSON.parse(data)
-		  res.writeHead(200, {"Content-Type": "text/html"});
-		  res.write(affiche_objet(obj));
-		    res.render('index.ejs', {adresse: resultat})
+			obj = JSON.parse(data)
+			console.log(obj);
+		  	res.render('index.ejs', {provinces: obj});
 		});
 })
 
-var server = app.listen(8082, function () {
+var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
-})
-
+   console.log('Server running.');
+});
